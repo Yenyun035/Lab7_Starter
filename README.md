c8bc8b34a209526d31ef549fd75808eae1f332a8
Yenyun035 <57857379+Yenyun035@users.noreply.github.com>
2021-11-06 00:34:14 -0700

    complete lab

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab 7 - Starter Code
+Yen-Yun Wu
--- sw.js
+++ sw.js
@@ -9,6 +9,22 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  let urls = [
+    'https://introweb.tech/assets/json/ghostCookies.json',
+    'https://introweb.tech/assets/json/birthdayCake.json',
+    'https://introweb.tech/assets/json/chocolateChip.json',
+    'https://introweb.tech/assets/json/stuffing.json',
+    'https://introweb.tech/assets/json/turkey.json',
+    'https://introweb.tech/assets/json/pumpkinPie.json'
+  ];
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log("Opened Cache");
+        return cache.addAll(urls);
+      })
+  );
 });
 
 /**
@@ -21,6 +37,7 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +46,24 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if(response) { return response; }
+
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status != 200 
+              || response.type != "basic") {
+              return response;
+            }
+
+            let repCache = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(event.request, repCache);
+            });
+            return response;
+        })
+      })
+    )
 });
